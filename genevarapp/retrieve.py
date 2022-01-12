92db1cce924262337e33358d7d21c0e123b4e862
ilovericenoodle <78513437+ilovericenoodle@users.noreply.github.com>
2022-01-12 17:04:20 -0500

    20220112

--- genevarapp/retrieve.py
+++
@@ -1,7 +0,0 @@
-import os
-file = open("input_location.txt")
-line = file.read()
-file.close()
-wholecommand = 'python VCFanalyse.py  -chr chr2 -vcf '+line+ ' -bins 100 -minlen 100'
-print(wholecommand)
-os.system(wholecommand)
